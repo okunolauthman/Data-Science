6b2f7ac85806ea82d44d14c7fd658563701c0107
Uthman Okunola <127902774+okunolauthman@users.noreply.github.com>
2026-02-07 00:01:57 -0500

    Added messages and print statement to demonstate understanding

--- iris_knn.py
+++ iris_knn.py
@@ -8,6 +8,7 @@ from sklearn.metrics import accuracy_score, classification_report
 
 # 1. Load Iris dataset
 iris = load_iris()
+print("\nIris dataset loaded successfully.")
 X = iris.data      # features: sepal length, sepal width, petal length, petal width
 y = iris.target    # labels: species
 
@@ -15,19 +16,24 @@ y = iris.target    # labels: species
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
+print("\nIris Data has been split into 80% training and  20% testing sets.")
+# Testing data is real data that the model is not allowed to see during training.
+print(X_train.shape, X_test.shape)
 
 # 3. Create kNN model (k = 5)
 knn = KNeighborsClassifier(n_neighbors=5)
 
 # 4. Train the model
 knn.fit(X_train, y_train)
+print("\nModel trained successfully.")
 
 # 5. Make predictions
 y_pred = knn.predict(X_test)
+print("\nPredictions made on test set.")
 
 # 6. Evaluate model
 accuracy = accuracy_score(y_test, y_pred)
-print("Model Accuracy:", accuracy)
+print("\nModel Accuracy:", accuracy)
 
 print("\nClassification Report:")
 print(classification_report(y_test, y_pred, target_names=iris.target_names))
